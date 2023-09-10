1066b45b6e23bd1a636d5445a54fd2c58886754e
Ariadna Frias <ariadna.812@gmail.com>
2023-09-10 15:24:40 +0200

    Added operators

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,11 +1,40 @@
 // See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Welcome to the C# calculator");
-Console.WriteLine("Enter your name: ");
+
+Console.WriteLine("Hello! Please enter your name: ");
 string username = Console.ReadLine();
+Console.WriteLine($"Welcome {username} to the C# calculator");
 
 
-
-Console.WriteLine("Enter the first number");
+Console.WriteLine("Enter the first number: ");
 double firstNumber = double.Parse(Console.ReadLine());
-Console.WriteLine($"{username}: {firstNumber}");
+
+Console.WriteLine("Enter the second number: ");
+double secondNumber = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter the operation that you want to calculate. Examples: +, -, *, / ");
+string operation = Console.ReadLine();
+
+double total;
+switch(operation)
+{
+  case "+":
+    total = firstNumber + secondNumber;
+    Console.WriteLine($"The sum between {firstNumber} and {secondNumber} is {total}.");
+    break;
+  case "-":
+    total = firstNumber - secondNumber;
+    Console.WriteLine($"The subtraction between {firstNumber} and {secondNumber} is {total}.");
+    break;
+  case "*":
+    total = firstNumber * secondNumber;
+    Console.WriteLine($"The multiplication operation between {firstNumber} and {secondNumber} is {total}.");
+    break;
+  case "/":
+    total = firstNumber / secondNumber;
+    Console.WriteLine($"The division between {firstNumber} and {secondNumber} is {total}.");
+    break;
+  default:
+    Console.WriteLine("Operation not valid. Please reload the program.");
+    break;
+}
